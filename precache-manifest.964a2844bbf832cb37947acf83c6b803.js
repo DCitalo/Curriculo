c63a6258c5700d942b54696c0aea39b0ec3d559b
Italo Campana Drago <idrago@t4f.com.br>
2020-06-10 19:08:40 -0300

    Updates

---
+++ precache-manifest.964a2844bbf832cb37947acf83c6b803.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be168db7108be2332f8c01c42f019a23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7423f6ff038b50c5f8ee",
+    "url": "/static/css/main.98fea022.chunk.css"
+  },
+  {
+    "revision": "0fe2b3d37cf1768b29d9",
+    "url": "/static/js/2.2ba8ef5c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.2ba8ef5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7423f6ff038b50c5f8ee",
+    "url": "/static/js/main.4babf420.chunk.js"
+  },
+  {
+    "revision": "55f9b83067609d933757",
+    "url": "/static/js/runtime-main.86bf242d.js"
+  }
+]);
